f42dba3c93326ef1f117d2287a5d25a7af46fc7c
AlMuz <almuz@inbox.lv>
2019-05-06 19:13:59 +0300

    paginator buttons and functionality added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="row" v-if="articles.data">
-    <div class="col-md-8 offset-md-2" v-for="article in articles.data" :key="article.id">
-      <Article :article="article" />
+  <div>
+    <div class="d-flex justify-content-between mt-4">
+      <button @click="getPreviousArticles()" :disabled="articles.prev_page_url === null" class="btn btn-warning"> Previous page </button>
+      <button @click="getNextArticles()" :disabled="articles.next_page_url === null" class="btn btn-warning"> Next page </button>
+    </div>
+    <div class="row" v-if="!loading">
+      <div class="col-md-8 offset-md-2" v-for="article in articles.data" :key="article.id">
+        <Article :article="article" />
+      </div>
+    </div>
+    <div class="loader text-center" v-else>
+      <i class="fas fa-5x fa-spin fa-spinner"></i>
     </div>
   </div>
 </template>
@@ -19,19 +28,36 @@ export default {
   },
   data() {
     return {
-      articles: {}
+      articles: {},
+      loading: true
     }
   },
   mounted() {
     this.getArticles();
   },
   methods: {
-    getArticles() {
-      Axios.get(getArticleUrl)
+    getArticles(url = getArticleUrl) {
+
+      this.loading = true;
+      Axios.get(url)
       .then(response => {
+
+        this.loading = false;
         this.articles = response.data.data;
       })
+    },
+    getNextArticles() {
+      this.getArticles(this.articles.next_page_url);
+    },
+    getPreviousArticles() {
+      this.getArticles(this.articles.prev_page_url);
     }
   }
 }
 </script>
+
+<style>
+.btn-warning {
+  color: #fff ;
+}
+</style>
